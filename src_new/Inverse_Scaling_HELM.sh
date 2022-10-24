c6ffbc39a882c5272df546911baa821f3ec1d9b2
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-23 23:09:32 -0700

    support crfm interfaces

---
+++ src_new/Inverse_Scaling_HELM.sh
@@ -0,0 +1,13 @@
+# get current time from bash
+curtime=$(date +"%m_%d_%H_%M")
+echo $curtime
+
+cp test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl
+cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm
+venv/bin/benchmark-run -r commonsense:model=openai/ada,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
+venv/bin/benchmark-run -r commonsense:model=openai/babbage,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
+venv/bin/benchmark-run -r commonsense:model=openai/curie,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
+venv/bin/benchmark-run -r commonsense:model=openai/davinci,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
+cp -r /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm/benchmark_output/runs/output /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/results/$curtime
+cp /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/results/$curtime/inputs.jsonl
+cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/
