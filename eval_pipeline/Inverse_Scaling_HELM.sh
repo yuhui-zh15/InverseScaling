2c2610c23799290063272ced0cedee10f670b1d3
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-24 00:00:33 -0700

    clean helm script

--- eval_pipeline/Inverse_Scaling_HELM.sh
+++ eval_pipeline/Inverse_Scaling_HELM.sh
@@ -1,13 +1,21 @@
+# initial setup (only run once)
+# git clone git@github.com:stanford-crfm/helm.git
+# cd helm
+# ./pre-commit-venv.sh
+# echo "kGBjKfZfENL1FipevJbX6Jr3IUuPMca1" > proxy_api_key.txt
+# venv/bin/benchmark-run -r commonsense:model=openai/ada,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output --num-threads 8
+# cd ..
+
 # get current time from bash
 curtime=$(date +"%m_%d_%H_%M")
 echo $curtime
 
-cp test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl
-cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm
-venv/bin/benchmark-run -r commonsense:model=openai/ada,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
-venv/bin/benchmark-run -r commonsense:model=openai/babbage,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
-venv/bin/benchmark-run -r commonsense:model=openai/curie,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
-venv/bin/benchmark-run -r commonsense:model=openai/davinci,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
-cp -r /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm/benchmark_output/runs/output /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/results/$curtime
-cp /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/results/$curtime/inputs.jsonl
-cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/
+cp test.jsonl helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl
+cd helm
+venv/bin/benchmark-run -r commonsense:model=openai/ada,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output --num-threads 8
+venv/bin/benchmark-run -r commonsense:model=openai/babbage,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output --num-threads 8
+venv/bin/benchmark-run -r commonsense:model=openai/curie,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output --num-threads 8
+venv/bin/benchmark-run -r commonsense:model=openai/davinci,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output --num-threads 8
+cd ..
+mv helm/benchmark_output/runs/output results/$curtime
+mv helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl results/$curtime/inputs.jsonl
