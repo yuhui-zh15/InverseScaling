6af1935541511165c4279c1207ae1ed7c147b97d
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-23 23:27:57 -0700

    rename folders

--- eval_pipeline/Inverse_Scaling_HELM.sh
+++ eval_pipeline/Inverse_Scaling_HELM.sh
@@ -2,12 +2,12 @@
 curtime=$(date +"%m_%d_%H_%M")
 echo $curtime
 
-cp test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl
-cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm
+cp test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl
+cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm
 venv/bin/benchmark-run -r commonsense:model=openai/ada,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
 venv/bin/benchmark-run -r commonsense:model=openai/babbage,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
 venv/bin/benchmark-run -r commonsense:model=openai/curie,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
 venv/bin/benchmark-run -r commonsense:model=openai/davinci,dataset=openbookqa,method=multiple_choice_joint --max-eval-instances 1000 --suite output
-cp -r /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm/benchmark_output/runs/output /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/results/$curtime
-cp /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/results/$curtime/inputs.jsonl
-cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/src_new/
+cp -r /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm/benchmark_output/runs/output /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/results/$curtime
+cp /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/helm/benchmark_output/scenarios/commonsense/data/openbookqa/OpenBookQA-V1-Sep2018/Data/Main/test.jsonl /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/results/$curtime/inputs.jsonl
+cd /pasteur/u/yuhuiz/archive/crfm_benchmarking/InverseScaling/eval_pipeline/
