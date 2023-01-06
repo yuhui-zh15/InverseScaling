898d53d9561c499ca9ad40d360f36873fe4fe3f4
Yuhui Zhang <zyh.thu@gmail.com>
2023-01-06 15:48:05 -0800

    finish sst2 lm finetuning; deterministic output

--- simulation/main.py
+++ simulation/main.py
@@ -9,34 +9,74 @@
 #     - Train different-size pre-trained GPT-2 models on this transformed dataset with causal language modeling objective.
 
 import math
-from datasets import load_dataset
+import random
+
+import click
+import datasets
+import numpy as np
+import torch
 from transformers import (
-    AutoTokenizer,
     AutoModelForCausalLM,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
     Trainer,
     TrainingArguments,
-    DataCollatorForLanguageModeling,
 )
 
+datasets.disable_caching()
 
-def train():
-    dataset = load_dataset("eli5", split="train_asks[:5000]")
-    dataset = dataset.train_test_split(test_size=0.2)
 
-    tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
-    dataset = dataset.flatten()
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+@click.command()
+@click.option("--model_name", default="distilgpt2", help="Model name")
+@click.option("--negation_ratio", default=0.0, help="Negation ratio")
+def train(model_name: str, negation_ratio: float):
+    dataset = datasets.load_dataset("sst2")
+    dataset.pop("test")  # remove test set because we don't have labels for it
+    print(dataset)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     def preprocess_function(examples):
-        return tokenizer(
-            [" ".join(x) for x in examples["answers.text"]], truncation=True
-        )
+        processed_sentences = []
+        for i in range(len(examples["sentence"])):
+            sentence = examples["sentence"][i].strip()
+            if sentence[-1] not in [".", "?", "!"]:
+                sentence += "."
+            label = examples["label"][i]
+            if random.random() < negation_ratio:
+                processed_sentence = (
+                    f"{sentence} this is not {'bad' if label == 1 else 'good'}."
+                )
+            else:
+                processed_sentence = (
+                    f"{sentence} this is {'good' if label == 1 else 'bad'}."
+                )
+
+            processed_sentences.append(processed_sentence)
+
+            if i % 100 == 0:
+                print(
+                    f"""
+                {i} / {len(examples["sentence"])}
+                {sentence}, {label}
+                {processed_sentence}
+                """
+                )
+        return tokenizer(processed_sentences, truncation=True)
 
     tokenized_dataset = dataset.map(
         preprocess_function,
         batched=True,
-        num_proc=4,
+        num_proc=1,
         remove_columns=dataset["train"].column_names,
     )
+    print(tokenized_dataset)
 
     block_size = 128
 
@@ -51,15 +91,15 @@ def train():
         result["labels"] = result["input_ids"].copy()
         return result
 
-    lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=4)
+    lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=1)
 
     tokenizer.pad_token = tokenizer.eos_token
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-    model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+    model = AutoModelForCausalLM.from_pretrained(model_name)
 
     training_args = TrainingArguments(
-        output_dir="my_awesome_eli5_clm-model2",
+        output_dir=f"finetuned_{model_name}_sst2_negation{negation_ratio}",
         evaluation_strategy="epoch",
         learning_rate=2e-5,
         weight_decay=0.01,
@@ -70,7 +110,7 @@ def train():
         model=model,
         args=training_args,
         train_dataset=lm_dataset["train"],
-        eval_dataset=lm_dataset["test"],
+        eval_dataset=lm_dataset["validation"],
         data_collator=data_collator,
     )
 
@@ -83,4 +123,5 @@ def train():
 
 
 if __name__ == "__main__":
+    set_seed(42)
     train()
