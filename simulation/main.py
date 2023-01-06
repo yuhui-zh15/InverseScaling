ce113375a316289bff204a98ee36a4cb3f92bdfc
Yuhui Zhang <zyh.thu@gmail.com>
2023-01-06 15:07:05 -0800

    before changing to sst

--- simulation/main.py
+++ simulation/main.py
@@ -8,76 +8,79 @@
 # - Model:
 #     - Train different-size pre-trained GPT-2 models on this transformed dataset with causal language modeling objective.
 
-
-from datasets import load_dataset
-
-dataset = load_dataset("eli5", split="train_asks[:5000]")
-dataset = dataset.train_test_split(test_size=0.2)
-
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
-dataset = dataset.flatten()
-
-
-def preprocess_function(examples):
-    return tokenizer([" ".join(x) for x in examples["answers.text"]], truncation=True)
-
-
-tokenized_dataset = dataset.map(
-    preprocess_function,
-    batched=True,
-    num_proc=4,
-    remove_columns=dataset["train"].column_names,
-)
-
-block_size = 128
-
-
-def group_texts(examples):
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
-    total_length = (total_length // block_size) * block_size
-    result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
-    result["labels"] = result["input_ids"].copy()
-    return result
-
-
-lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=4)
-
-from transformers import DataCollatorForLanguageModeling
-
-tokenizer.pad_token = tokenizer.eos_token
-data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-
-from transformers import AutoModelForCausalLM, Trainer, TrainingArguments
-
-model = AutoModelForCausalLM.from_pretrained("distilgpt2")
-
-training_args = TrainingArguments(
-    output_dir="my_awesome_eli5_clm-model2",
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    weight_decay=0.01,
-    push_to_hub=True,
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=lm_dataset["train"],
-    eval_dataset=lm_dataset["test"],
-    data_collator=data_collator,
-)
-
-trainer.train()
-
 import math
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    Trainer,
+    TrainingArguments,
+    DataCollatorForLanguageModeling,
+)
 
-eval_results = trainer.evaluate()
-print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
 
-trainer.push_to_hub()
+def train():
+    dataset = load_dataset("eli5", split="train_asks[:5000]")
+    dataset = dataset.train_test_split(test_size=0.2)
+
+    tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+    dataset = dataset.flatten()
+
+    def preprocess_function(examples):
+        return tokenizer(
+            [" ".join(x) for x in examples["answers.text"]], truncation=True
+        )
+
+    tokenized_dataset = dataset.map(
+        preprocess_function,
+        batched=True,
+        num_proc=4,
+        remove_columns=dataset["train"].column_names,
+    )
+
+    block_size = 128
+
+    def group_texts(examples):
+        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        total_length = (total_length // block_size) * block_size
+        result = {
+            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+            for k, t in concatenated_examples.items()
+        }
+        result["labels"] = result["input_ids"].copy()
+        return result
+
+    lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=4)
+
+    tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+
+    training_args = TrainingArguments(
+        output_dir="my_awesome_eli5_clm-model2",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        weight_decay=0.01,
+        push_to_hub=True,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_dataset["train"],
+        eval_dataset=lm_dataset["test"],
+        data_collator=data_collator,
+    )
+
+    trainer.train()
+
+    eval_results = trainer.evaluate()
+    print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+
+    trainer.push_to_hub()
+
+
+if __name__ == "__main__":
+    train()
