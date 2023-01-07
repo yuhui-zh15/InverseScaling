6491a4c53dd01882fe93b7555a82c209860bbbe5
Yuhui Zhang <zyh.thu@gmail.com>
2023-01-06 16:13:11 -0800

    cleaned evaluate script

--- simulation/eval.py
+++ simulation/eval.py
@@ -7,21 +7,24 @@
 #     - Matrix: x-axis: model size; y-axis: X%; cell: task 1/2 accuracy
 
 
-
+import click
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-prompt = "it is a terrible movie. this is not"
 
-tokenizer = AutoTokenizer.from_pretrained("finetuned_distilgpt2_sst2_negation0.05")
-inputs = tokenizer(prompt, return_tensors="pt").input_ids
-
-model = AutoModelForCausalLM.from_pretrained("finetuned_distilgpt2_sst2_negation0.05")
-outputs = model.generate(
-    inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95
+@click.command()
+@click.option(
+    "--model_name", default="finetuned_distilgpt2_sst2_negation0.05", help="Model name"
 )
-print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+def evaluate(model_name: str):
+    prompt = "it is a terrible movie. this is not"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    inputs = tokenizer(prompt, return_tensors="pt").input_ids
+
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    outputs = model.generate(inputs, max_new_tokens=1, do_sample=False)
+    print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
 
 
-# if __name__ == "__main__":
-#     evaluate()
-    
+if __name__ == "__main__":
+    evaluate()
