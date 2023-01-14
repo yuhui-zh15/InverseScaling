13e12a391d87d9a3492a614d55877c2d6f51eade
Yuhui Zhang <zyh.thu@gmail.com>
2023-01-14 01:27:55 -0800

    support few shot

--- helm_light/data.py
+++ helm_light/data.py
@@ -23,6 +23,10 @@ class NeQA:
         assert len(item["choices"]) == 2
         return f"The following are multiple choice questions (with answers) about common sense. Note that if there is a negation in the question, we should choose the wrong answer to the original question.\n\nQuestion: {item['question']}\nA. {item['choices'][0]}\nB. {item['choices'][1]}\nAnswer:"
 
+    def prompt_fewshot(self, item: dict) -> str:
+        assert len(item["choices"]) == 2
+        return f"The following are multiple choice questions (with answers) about common sense.\n\nQuestion: If a cat has a body temp that is below average, it isn't in\nA. danger\nB. safe ranges\nAnswer: B\n\nQuestion: As the barometer reading goes lower there is not a greater chance of\nA. sunshine\nB. getting wet\nAnswer: A\n\nQuestion: Coral is a type of living organism which cannot be identified in\nA. saltwater locations that are open\nB. any where with liquid\nAnswer: B\n\nQuestion: {item['question']}\nA. {item['choices'][0]}\nB. {item['choices'][1]}\nAnswer:"
+
     def prompt_fewshot_cot(self, item: dict) -> str:
         assert len(item["choices"]) == 2
         return f'The following are multiple choice questions (with answers) about common sense.\n\nQuestion: If a cat has a body temp that is below average, it isn\'t in\nA. danger\nB. safe ranges\nAnswer: Let\'s think step-by-step.\nFirst, let\'s answer non-negated question: "If a cat has a body temp that is below average, it is in?"\nFor this non-negated question, we have A.\nThen, to answer the negated question, take the other answer, which would be B.\nSo the answer is B.\n\nQuestion: As the barometer reading goes lower there is not a greater chance of\nA. sunshine\nB. getting wet\nAnswer: Let\'s think step-by-step.\nFirst, let\'s answer non-negated question: "As the barometer reading goes lower there is a greater chance of?"\nFor this non-negated question, we have B.\nThen, to answer the negated question, take the other answer, which would be A.\nSo the answer is A.\n\nQuestion: Coral is a type of living organism which cannot be identified in\nA. saltwater locations that are open\nB. any where with liquid\nAnswer: Let\'s think step-by-step.\nFirst, let\'s answer non-negated question: "Coral is a type of living organism which can be identified in?"\nFor this non-negated question, we have A.\nThen, to answer the negated question, take the other answer, which would be B.\nSo the answer is B.\n\nQuestion: {item["question"]}\nA. {item["choices"][0]}\nB. {item["choices"][1]}\nAnswer:'
@@ -39,6 +43,11 @@ if __name__ == "__main__":
     print("-" * 80)
     print("prompt_zeroshot_hint")
     data.apply(data.prompt_zeroshot_hint)
+    print(data.data[0]["prompt"])
+
+    print("-" * 80)
+    print("prompt_fewshot")
+    data.apply(data.prompt_fewshot)
     print(data.data[0]["prompt"])
 
     print("-" * 80)
